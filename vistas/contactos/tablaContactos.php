c6d4f400088c79bdd425b373796ebb7892d47a7c
Posi-10 <mapiposi15@outlook.com>
2021-05-07 03:13:20 -0500

    C_15

--- vistas/contactos/tablaContactos.php
+++ vistas/contactos/tablaContactos.php
@@ -1,3 +1,22 @@
+<?php
+  require_once "../../clases/Conexion.php";
+  $con = new Conexion();
+  $conexion = $con -> conectar();
+    $sql = "SELECT 
+                contactos.paterno AS paterno,
+                contactos.materno AS materno,
+                contactos.nombre AS nombre,
+                contactos.telefono AS telefono,
+                contactos.email AS email,
+                categoria.nombre AS categoria,
+                contactos.id_agenda AS  id_agenda
+            FROM
+                t_contactos AS contactos
+            INNER JOIN
+                t_categorias AS categoria
+            ON contactos.id_categoria = categoria.id_categoria;";
+  $r = mysqli_query($conexion, $sql);
+?>
 <div class="card">
   <div class="card-body">
     <div class="table-responsive">
@@ -15,24 +34,31 @@
                 </tr>
             </thead>
             <tbody>
+                <?php
+                    while($mostrar = mysqli_fetch_array($r)){
+                        $id_agenda = $mostrar['id_agenda'];
+                ?>
                 <tr style="text-align: center;">
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td><?php echo $mostrar['paterno']; ?></td>
+                    <td><?php echo $mostrar['materno']; ?></td>
+                    <td><?php echo $mostrar['nombre']; ?></td>
+                    <td><?php echo $mostrar['telefono']; ?></td>
+                    <td><?php echo $mostrar['email']; ?></td>
+                    <td><?php echo $mostrar['categoria']; ?></td>
                     <td>
-                        <span class="btn btn-outline-success btn-sm" data-toggle="modal" data-target="#modalActualizarContacto">
+                        <span class="btn btn-outline-success btn-sm" onclick="obtenerDatosContacto('<?php  echo $id_agenda ?>')" data-toggle="modal" data-target="#modalActualizarContacto">
                             <span class="fas fa-edit"></span>
                         </span>
                     </td>
                     <td>
-                        <span class="btn btn-outline-danger btn-sm" onclick="eliminarConctacto()">
+                        <span class="btn btn-outline-danger btn-sm" onclick="eliminarConctacto('<?php  echo $id_agenda ?>')">
                             <span class="fas fa-trash"></span>
                         </span>
                     </td>
                 </tr>
+                <?php
+                    }
+                ?>
             </tbody>
         </table>
     </div>
